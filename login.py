8c3df3b3e91f5c28e0f39afcb6853aadd12a2c59
mzwandile <stuurmanmzwandile@gmail.com>
2021-06-14 16:35:21 +0200

    second commit

--- login.py
+++ login.py
@@ -6,72 +6,81 @@ root.geometry("500x500")
 
 root.config(bg="dark slate grey")
 
- #Name and Surname labels and entries
-name_label = Label(root, text="Enter your name")
-name_label.place(x=10,y=10)
-surname_label = Label(root, text="Enter Surname")
-surname_label.place(x=10,y=50)
-name_entry= Entry(root)
-name_entry.place(x=100,y=10)
-surname_entry = Entry(root)
-surname_entry.place(x=100,y=50)
+class Login():
 
-email_label = Label(root, text="Enter your email: ")
-email_label.place(x=10, y=100)
-email_entry = Entry(root)
-email_entry.place(x=100,y=100)
+    def __init__(self,master):
+        #Name and Surname labels and entries
+        self.name_label = Label(master, text="Enter your name")
+        self.name_label.place(x=10,y=10)
+        self.surname_label = Label(master, text="Enter Surname")
+        self.surname_label.place(x=10,y=50)
+        self.name_entry= Entry(master)
+        self.name_entry.place(x=150,y=10)
+        self.surname_entry = Entry(master)
+        self.surname_entry.place(x=150,y=50)
 
-
-
-id_entry = Entry(root)
-id_entry.config(state='readonly')
-id_entry.place(x=150,y=150)
+        self.email_label = Label(master, text="Enter your email: ")
+        self.email_label.place(x=10, y=100)
+        self.email_entry = Entry(master)
+        self.email_entry.place(x=150,y=100)
 
 
 
-def details():
-    name = name_entry.get()
-    surname = surname_entry.get()
-    email = str(email_entry.get())
+        self.id_entry = Entry(master)
+        self.id_entry.config(state='readonly')
+        self.id_entry.place(x=200,y=150)
+
+        self.email_validator = Button(root, text="Enter Id number: ", command=self.details)
+        self.email_validator.place(x=10, y=150)
+
+        self.login_btn = Button(root, text ="login", command=self.id_check)
+        self.login_btn.place(x=10, y=250)
 
 
-    if "@" in email_entry.get():
-        id_entry.config(state='normal')
 
-    else:
-        messagebox.showinfo(title='Alert!', message="Please enter valid email adress!")
-        id_entry.config(state='readonly')
-        email_entry.delete(0,END)
+    def details(self):
+        name = self.name_entry.get()
+        surname = self.surname_entry.get()
+        email = str(self.email_entry.get())
 
-def id_check():
-    try:
-        Id = int(id_entry.get())
-        id_ls = id_entry.get()
 
-        year = id_entry.get()[1]
-        year2 = id_entry.get()[0:2]
+        if "@" in self.email_entry.get():
+            self.id_entry.config(state='normal')
 
-        if type(Id) == type(str()) or len(id_ls) != 13:
-            raise ValueError
-        elif int(year) <= 3:
-            messagebox.showinfo(title="Play!", message="Lets Play!")
-            import lotto
-            root.destroy()
-        elif int(year2) >3 and int(year2) > 21:
-            messagebox.showinfo(title="Play!", message="Lets Play!")
-            import lotto
-            root.destroy()
         else:
-            #x = int(year) -3
-            messagebox.showinfo(title="Under Age", message="Your are too young to play")
-    except ValueError:
-        messagebox.showerror(title="Invalid Id", message="Please enter valid ID")
-        id_entry.delete(0,END)
+            messagebox.showinfo(title='Alert!', message="Please enter valid email adress!")
+            self.id_entry.config(state='readonly')
+            self.email_entry.delete(0,END)
 
-email_validator = Button(root, text="Enter Id number: ", command=details)
-email_validator.place(x=10, y=150)
+    def id_check(self):
+        try:
+            Id = int(self.id_entry.get())
+            id_ls = self.id_entry.get()
 
-login_btn = Button(root, text ="login", command=id_check)
-login_btn.place(x=10, y=250)
+            year = self.id_entry.get()[1]
+            year2 = self.id_entry.get()[0:2]
+
+            if type(Id) == type(str()) or len(id_ls) != 13:
+                raise ValueError
+            elif int(year) <= 3:
+
+                messagebox.showinfo(title="Play!", message="Lets Play!")
+                import lotto
+                root.destroy()
+            elif int(year2) >3 and int(year2) > 21:
+                messagebox.showinfo(title="Play!", message="Lets Play!")
+                import lotto
+                root.destroy()
+            else:
+                #x = int(year) -3
+                messagebox.showinfo(title="Under Age", message="Your are too young to play")
+        except ValueError:
+
+            messagebox.showerror(title="Invalid Id", message="Please enter valid ID")
+            self.id_entry.delete(0,END)
+
+
+
+x=Login(root)
 root.mainloop()
 
--- lotto.py
+++ lotto.py
@@ -7,11 +7,14 @@ root.title("lotto")
 root.config(bg="yellow")
 
 class Lotto_machine():
-    def __init__(self):
+        def __init__(self,slave):
+            self.heading_label = Label(slave, text = "Pick 6 numbers", font="Consolas", bg="white")
+            self.heading_label.place(x=150,y=10)
 
 
 
 
 
 
+y=Lotto_machine(root)
 root.mainloop()
